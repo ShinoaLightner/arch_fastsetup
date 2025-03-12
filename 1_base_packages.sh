4c1cd207da37ea2355e52d99782e7cb40f194a5f
Vladyslav <53900308+ShinoaLightner@users.noreply.github.com>
2025-03-12 10:57:59 +0200

    Added to base_packages choosing graphic drivers

--- 1_base_packages.sh
+++ 1_base_packages.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Select video driver
+echo "Select your GPU driver:"
+echo "1) NVIDIA"
+echo "2) Intel"
+echo "3) AMD"
+read -p "Enter the number (1-3): " gpu_choice
+
+case $gpu_choice in
+    1) video_driver="nvidia-open nvidia-utils nvidia-settings" ;;
+    2) video_driver="mesa xf86-video-intel" ;;
+    3) video_driver="mesa xf86-video-amdgpu" ;;
+    *) echo "Invalid choice. Defaulting to No Graphic Driver."; video_driver="" ;;
+esac
+
+
 echo "### Installing the base system..."
-pacstrap -K /mnt base linux linux-firmware networkmanager pipewire pipewire-pulse grub efibootmgr alacritty bspwm rofi polybar dunst picom sddm nano discord firefox sudo git
+pacstrap -K /mnt base linux linux-firmware networkmanager pipewire pipewire-pulse grub efibootmgr alacritty bspwm rofi polybar dunst picom sddm nano discord firefox sudo git $video_driver
 echo "✅ Base system installed!"
 read -p "Press [Enter] to continue..."
 
@@ -10,4 +25,4 @@ genfstab -U /mnt >> /mnt/etc/fstab
 echo "✅ fstab generated!"
 read -p "Press [Enter] to continue..."
 
-mv after_arch_chroot.sh /mnt/after_arch_chroot.sh
+mv 2_after_arch_chroot.sh /mnt/after_arch_chroot.sh
